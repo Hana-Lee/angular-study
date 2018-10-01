bbf423b9b27ba567f8c34905577430983332362a
Hana Lee <i@leehana.co.kr>
2018-10-01 15:49:53 +0900

    * app title change angular-demo to angular-study * change indent setting space to tab * add .iml file to .gitignore

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# AngularDemo
+# Angular Study
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 6.2.3.
 
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -13,15 +13,15 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it(`should have as title 'angular-demo'`, async(() => {
+  it(`should have as title 'angular-study'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('angular-demo');
+    expect(app.title).toEqual('angular-study');
   }));
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to angular-demo!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to angular-study!');
   }));
 });
